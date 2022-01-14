c6fd35b5fd2d8324fed498c231ddfa4b56f07c3a
SUIBING112233 <suibing112233@outlook.com>
2022-01-14 19:12:39 +0800

    🔨 Refact: Remove unuse import

--- src/main/java/com/catkatpowered/katserver/network/WebSocketHandler.java
+++ src/main/java/com/catkatpowered/katserver/network/WebSocketHandler.java
@@ -2,7 +2,6 @@ package com.catkatpowered.katserver.network;
 
 
 import io.javalin.websocket.WsMessageContext;
-import io.javalin.websocket.WsMessageHandler;
 
 public class WebSocketHandler {
     public static void WebSocketMessageHandler(WsMessageContext ctx) {
