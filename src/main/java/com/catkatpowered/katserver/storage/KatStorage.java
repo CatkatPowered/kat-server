f23314eeb608ead66765dee558ccb9329e30e258
hanbings <3219065882@qq.com>
2022-01-17 11:29:54 +0800

    ✨ Feat: Create ShaUtils and remove BouncyCastle depend.

--- src/main/java/com/catkatpowered/katserver/storage/KatStorage.java
+++ src/main/java/com/catkatpowered/katserver/storage/KatStorage.java
@@ -1,13 +1,9 @@
 package com.catkatpowered.katserver.storage;
 
-import java.security.Security;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-
 public class KatStorage {
 
     public static void KatStorageMain() {
-        // 注册BouncyCastle算法库
-        Security.addProvider(new BouncyCastleProvider());
+        // ShaUtils 轮子已造
 
     }
 }
---
+++ src/main/java/com/catkatpowered/katserver/util/ShaUtils.java
@@ -0,0 +1,80 @@
+package com.catkatpowered.katserver.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * sha256 辅助工具类
+ *
+ * @author hanbings
+ */
+public class ShaUtils {
+    public String sha256(File file) {
+        if (file == null || file.length() == 0) {
+            return null;
+        }
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("Sha256");
+            FileInputStream fileInputStream = new FileInputStream(file);
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = fileInputStream.read(buffer)) != -1) {
+                messageDigest.update(buffer, 0, len);
+            }
+            fileInputStream.close();
+            byte[] byteArray = messageDigest.digest();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (byte temp : byteArray) {
+                stringBuilder.append(String.format("%02x", temp));
+            }
+            return stringBuilder.toString();
+        } catch (IOException | NoSuchAlgorithmException exception) {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+    public String sha256(InputStream inputStream) {
+        if (inputStream == null) {
+            return null;
+        }
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("Sha256");
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = inputStream.read(buffer)) != -1) {
+                messageDigest.update(buffer, 0, len);
+            }
+            inputStream.close();
+            byte[] byteArray = messageDigest.digest();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (byte temp : byteArray) {
+                stringBuilder.append(String.format("%02x", temp));
+            }
+            return stringBuilder.toString();
+        } catch (IOException | NoSuchAlgorithmException exception) {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+    public String sha256(byte[] bytes) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("Sha256");
+            messageDigest.update(bytes);
+            byte[] byteArray = messageDigest.digest();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (byte temp : byteArray) {
+                stringBuilder.append(String.format("%02x", temp));
+            }
+            return stringBuilder.toString();
+        } catch (NoSuchAlgorithmException exception) {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+}
