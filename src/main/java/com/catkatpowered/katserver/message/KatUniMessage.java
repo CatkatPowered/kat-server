5574c14ebd2d845a0ef6b84b60128a1d0b7db64f
SUIBING112233 <suibing112233@outlook.com>
2022-01-20 19:36:54 +0800

    ✨ Feat: Add @SerializedName()

--- src/main/java/com/catkatpowered/katserver/message/KatUniMessage.java
+++ src/main/java/com/catkatpowered/katserver/message/KatUniMessage.java
@@ -1,6 +1,7 @@
 package com.catkatpowered.katserver.message;
 
 import com.catkatpowered.katserver.common.KatMessageTypeConstants;
+import com.google.gson.annotations.SerializedName;
 import java.util.ArrayList;
 import lombok.Builder;
 import lombok.Builder.Default;
@@ -14,19 +15,27 @@ import lombok.Builder.Default;
 public class KatUniMessage {
 
     @Default
+    @SerializedName("message_type")
     public String messageType = KatMessageTypeConstants.KAT_MESSAGE_TYPE_PLAIN_MESSAGE;
 
+    @SerializedName("message_id")
     public String messageID;
 
+    @SerializedName("message_content")
     public ArrayList<Byte> messageContent;
 
+    @SerializedName("message_list")
     public ArrayList<KatUniMessage> messageList;
 
+    @SerializedName("extended")
     public ArrayList<Byte> extended;
 
+    @SerializedName("media_hash")
     public String mediaHash;
 
+    @SerializedName("media_name")
     public String mediaName;
 
+    @SerializedName("media_url")
     public String mediaURL;
 }
