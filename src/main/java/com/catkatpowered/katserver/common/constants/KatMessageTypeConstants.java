50c8afff3153ce921b9f8bf353f238dab3b1c825
SUIBING112233 <suibing112233@outlook.com>
2022-01-23 14:38:54 +0800

    ✨ Feat: Add KatMessageTypeConstants.KAT_MESSAGE_TYPE_AUDIO_MESSAGE KatMessageTypeConstants.KAT_MESSAGE_TYPE_FILE_MESSAGE KatMessageTypeConstants.KAT_MESSAGE_TYPE_MIXED_MESSAGE

--- src/main/java/com/catkatpowered/katserver/common/constants/KatMessageTypeConstants.java
+++ src/main/java/com/catkatpowered/katserver/common/constants/KatMessageTypeConstants.java
@@ -4,6 +4,9 @@ public class KatMessageTypeConstants {
 
     public static final String KAT_MESSAGE_TYPE_PLAIN_MESSAGE = "PlainMessage";
     public static final String KAT_MESSAGE_TYPE_COLLECTION_MESSAGE = "CollectionMessage";
-    public static final String KAT_MESSAGE_TYPE_MEDIA_MESSAGE = "MediaMessage";
+    public static final String KAT_MESSAGE_TYPE_IMAGE_MESSAGE = "ImageMessage";
     public static final String KAT_MESSAGE_TYPE_FILE_MESSAGE = "FileMessage";
+    public static final String KAT_MESSAGE_TYPE_AUDIO_MESSAGE = "AudioMessage";
+    public static final String KAT_MESSAGE_TYPE_VIDEO_MESSAGE = "VideoMessage";
+    public static final String KAT_MESSAGE_TYPE_MIXED_MESSAGE = "MixedMessage";
 }
