8dcbc32a72d9dbf770865f407260c843c84198c0
SUIBING112233 <suibing112233@outlook.com>
2022-01-23 15:06:03 +0800

    📝 Docs: Add document for KatUniMessage;Add more MessageType into KatUniMessageType

--- src/main/java/com/catkatpowered/katserver/message/KatUniMessage.java
+++ src/main/java/com/catkatpowered/katserver/message/KatUniMessage.java
@@ -14,38 +14,81 @@ import lombok.Data;
 @Data
 public class KatUniMessage {
 
+    /**
+     * <b>MessageType</b> 用于区分本级的消息类型，方便前端处理消息的展示以及<em>KatServer</em>对消息的储存和处理。<br>
+     * <p>
+     * 其中为了方便<em>Extension</em>的识别，这里采用的是字符串储存而不是<b>enum</b>。 <br>
+     * <p>
+     * 同时提供了<b>KatUniMessageTypeManager</b>方便<em>Extension</em>拓展消息的类型。
+     *
+     * @see KatMessageTypeConstants
+     * @see KatUniMessageTypeManager
+     */
     @SerializedName("message_type")
     public String messageType = KatMessageTypeConstants.KAT_MESSAGE_TYPE_PLAIN_MESSAGE;
 
+    /**
+     * <b>MessageID</b> 是本级消息的唯一标识符，方便<em>Extension</em>对本层消息的处理以及<em>KatServer</em>对本层消息的存取。</br>
+     * <p>
+     * <b>MessageID</b> 由<em>KatServer</em>提供算法进行分配。暂定为<em>UUID</em></br>
+     */
     @SerializedName("message_id")
     public String messageID;
 
+    /**
+     * <b>MessageContent</b> 是本级消息的内容。<br>
+     */
     @SerializedName("message_content")
     public ArrayList<String> messageContent;
 
+    /**
+     * <b>MessageList</b> 指向的是下一级消息列表。</br>
+     * <p>
+     * 当前假设情况如下</br>
+     * <ol>
+     *     <li>如果<b>MessageType</b>为<b>KAT_MESSAGE_TYPE_MIXED_MESSAGE</b>，则<b>MessageList</b>中为顺序化的呈现</li>
+     *     <li>如果<b>MessageType</b>为<b>KAT_MESSAGE_TYPE_COLLECTION_MESSAGE</b>，则<b>MessageList</b>中为消息转发消息的列表</li>
+     * </ol>
+     */
     @SerializedName("message_list")
     public ArrayList<KatUniMessage> messageList;
 
+    /**
+     * <b>Extended</b> 是<em>Extension</em>对消息的额外补充内容</br>
+     * <p>
+     * <em>KatServer</em>对这部分消息是不敏感的，也就是说并不会对这段内容进行额外的补充
+     */
     @SerializedName("extended")
     public ArrayList<String> extended;
 
-    @SerializedName("media_hash")
-    public String mediaHash;
+    /**
+     * <b>ResourceHash</b> 是资源文件的哈希值
+     */
+    @SerializedName("resource_hash")
+    public String resourceHash;
 
-    @SerializedName("media_name")
-    public String mediaName;
+    /**
+     * <b>ResourceName</b> 为资源文件的实际名称
+     */
+    @SerializedName("resource_name")
+    public String resourceName;
 
-    @SerializedName("media_url")
-    public String mediaURL;
+    /**
+     * <b>ResourceURL</b> 为资源文件的下载地址
+     */
+    @SerializedName("resource_url")
+    public String resourceURL;
 
-    public KatUniMessage(String messageType, String messageID, ArrayList<String> messageContent, ArrayList<KatUniMessage> messageList, ArrayList<String> extended, String mediaHash, String mediaName, String mediaURL) {
+    public KatUniMessage(String messageType, String messageID, ArrayList<String> messageContent,
+        ArrayList<KatUniMessage> messageList, ArrayList<String> extended, String resourceHash,
+        String resourceName, String resourceURL) {
         this.messageType = messageType;
         this.messageID = messageID;
         this.messageContent = messageContent;
         this.messageList = messageList;
         this.extended = extended;
-        this.mediaHash = mediaHash;
-        this.mediaName = mediaName;
-        this.mediaURL = mediaURL;
+        this.resourceHash = resourceHash;
+        this.resourceName = resourceName;
+        this.resourceURL = resourceURL;
     }
 }
--- src/main/java/com/catkatpowered/katserver/message/KatUniMessageType.java
+++ src/main/java/com/catkatpowered/katserver/message/KatUniMessageType.java
@@ -14,23 +14,25 @@ public class KatUniMessageType {
 
     private static final KatUniMessageType Instance = new KatUniMessageType();
 
-    @Getter @Setter
+    @Getter
+    @Setter
     private HashSet<String> messageTypes = new HashSet<>();
 
     private KatUniMessageType() {
         this.messageTypes.add(KatMessageTypeConstants.KAT_MESSAGE_TYPE_FILE_MESSAGE);
-        this.messageTypes.add(KatMessageTypeConstants.KAT_MESSAGE_TYPE_MEDIA_MESSAGE);
         this.messageTypes.add(KatMessageTypeConstants.KAT_MESSAGE_TYPE_COLLECTION_MESSAGE);
         this.messageTypes.add(KatMessageTypeConstants.KAT_MESSAGE_TYPE_PLAIN_MESSAGE);
-
+        this.messageTypes.add(KatMessageTypeConstants.KAT_MESSAGE_TYPE_AUDIO_MESSAGE);
+        this.messageTypes.add(KatMessageTypeConstants.KAT_MESSAGE_TYPE_IMAGE_MESSAGE);
+        this.messageTypes.add(KatMessageTypeConstants.KAT_MESSAGE_TYPE_VIDEO_MESSAGE);
+        this.messageTypes.add(KatMessageTypeConstants.KAT_MESSAGE_TYPE_MIXED_MESSAGE);
     }
 
     public static KatUniMessageType getInstance() {
         return Instance;
     }
 
-
-    public  boolean addNewMessageType(String msgType) {
+    public boolean addNewMessageType(String msgType) {
         return this.messageTypes.add(msgType);
     }
 }
