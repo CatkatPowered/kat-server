4d6a2417b76a965632f82dd03a79b62ced28a0a1
SUIBING112233 <suibing112233@outlook.com>
2022-02-13 19:03:00 +0800

    🔨 Refact: Add @Experimental

--- src/main/java/com/catkatpowered/katserver/storage/KatMessageStorage.java
+++ src/main/java/com/catkatpowered/katserver/storage/KatMessageStorage.java
@@ -1,8 +1,10 @@
 package com.catkatpowered.katserver.storage;
 
+import jdk.jfr.Experimental;
 import lombok.extern.slf4j.Slf4j;
 
+@Experimental
 @Slf4j
-public class MessageStorage {
+public class KatMessageStorage {
 
 }
