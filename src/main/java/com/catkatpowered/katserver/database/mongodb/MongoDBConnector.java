a3557e83352d486fa9a9be10d934e69f4822e29b
hanbings <3219065882@qq.com>
2022-02-16 22:08:15 +0800

    🐛 Fix: Fix url port parsing error.

--- src/main/java/com/catkatpowered/katserver/database/mongodb/MongoDBConnector.java
+++ src/main/java/com/catkatpowered/katserver/database/mongodb/MongoDBConnector.java
@@ -6,7 +6,12 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
+import lombok.extern.slf4j.Slf4j;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Slf4j
 @SuppressWarnings("SpellCheckingInspection")
 public class MongoDBConnector implements DatabaseConnector {
 
@@ -15,17 +20,22 @@ public class MongoDBConnector implements DatabaseConnector {
 
     @Override
     public void loadDatabase(String url, String username, String password) {
-        MongoClientURI uri = new MongoClientURI(url);
-        connection = new MongoClient(
-                new ServerAddress(uri.getURI()),
-                MongoCredential.createCredential(
-                        uri.getUsername() != null ? uri.getUsername() : username,
-                        uri.getDatabase() != null ? uri.getDatabase() : "test",
-                        uri.getPassword() != null ? uri.getPassword() : password.toCharArray()
-                ),
-                uri.getOptions()
-        );
-        database = uri.getDatabase();
+        MongoClientURI resource = new MongoClientURI(url);
+        try {
+            URI uri = new URI(url);
+            connection = new MongoClient(
+                    uri.getPort() == -1 ? new ServerAddress(uri.getHost()) : new ServerAddress(uri.getHost(), uri.getPort()),
+                    MongoCredential.createCredential(
+                            resource.getUsername() != null ? resource.getUsername() : username,
+                            resource.getDatabase() != null ? resource.getDatabase() : "test",
+                            resource.getPassword() != null ? resource.getPassword() : password.toCharArray()
+                    ),
+                    resource.getOptions()
+            );
+            database = resource.getDatabase();
+        } catch (URISyntaxException exception) {
+            log.error(exception.getMessage());
+        }
     }
 
     @Override
--- src/test/java/com/catkatpowered/katserver/database/MongoDBDatabaseTest.java
+++ src/test/java/com/catkatpowered/katserver/database/MongoDBDatabaseTest.java
@@ -1,18 +1,26 @@
 package com.catkatpowered.katserver.database;
 
+import com.catkatpowered.katserver.KatServer;
 import com.catkatpowered.katserver.database.mongodb.MongoDBConnector;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 @SuppressWarnings("SpellCheckingInspection")
 public class MongoDBDatabaseTest {
+
+    // 若配置文件可读取则使用配置文件中的配置进行测试
+    String url = KatServer.KatConfigAPI.getConfig("database_url") == null
+            ? "mongodb://localhost:27017/kat-server"
+            : KatServer.KatConfigAPI.getConfig("database_url").toString();
+    String username = KatServer.KatConfigAPI.getConfig("database_username") == null
+            ? "kat-server"
+            : KatServer.KatConfigAPI.getConfig("database_username").toString();
+    String password = KatServer.KatConfigAPI.getConfig("database_password") == null
+            ? "kat-server"
+            : KatServer.KatConfigAPI.getConfig("database_password").toString();
+
     @Test
     public void connect() {
         MongoDBConnector connector = new MongoDBConnector();
-        connector.loadDatabase("mongodb://localhost:27017/kat-server",
-                "kat-server",
-                "kat-server");
-        assertNotNull(connector.getConnection().getMongoConnection());
+        connector.loadDatabase(url, username, password);
     }
 }
