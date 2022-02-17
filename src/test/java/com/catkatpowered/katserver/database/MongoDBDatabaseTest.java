e6ade5630feb7c9acc4337dc4dffec865656511a
hanbings <3219065882@qq.com>
2022-02-17 17:18:25 +0800

    ✔️ Feat: Add MongoDB test Actions and code.

---
+++ src/test/java/com/catkatpowered/katserver/database/MongoDBDatabaseTest.java
@@ -0,0 +1,19 @@
+package com.catkatpowered.katserver.database;
+
+import com.catkatpowered.katserver.database.mongodb.MongoDBConnector;
+import com.mongodb.client.MongoDatabase;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("SpellCheckingInspection")
+public class MongoDBDatabaseTest {
+    @Test
+    public void connect() {
+        // 生成连接器
+        MongoDBConnector connector = new MongoDBConnector();
+        connector.loadDatabase("mongodb://localhost:27017/kat-server", null, null);
+
+        // 创建数据库以测试连接
+        MongoDatabase database = connector.getConnection().getMongoConnection();
+        database.createCollection("kat-server");
+    }
+}
