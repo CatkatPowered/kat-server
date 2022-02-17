704f5323073a2b7db0edd286ab605702bc1c035a
hanbings <3219065882@qq.com>
2022-02-17 16:27:41 +0800

    🐛 Fix: Execute mongo connector null username and password input.

--- src/main/java/com/catkatpowered/katserver/database/mongodb/MongoDBConnector.java
+++ src/main/java/com/catkatpowered/katserver/database/mongodb/MongoDBConnector.java
@@ -21,21 +21,39 @@ public class MongoDBConnector implements DatabaseConnector {
     @Override
     public void loadDatabase(String url, String username, String password) {
         MongoClientURI resource = new MongoClientURI(url);
+        String host = "127.0.0.1";
+        int port = 27017;
+
         try {
             URI uri = new URI(url);
-            connection = new MongoClient(
-                    uri.getPort() == -1 ? new ServerAddress(uri.getHost()) : new ServerAddress(uri.getHost(), uri.getPort()),
-                    MongoCredential.createCredential(
-                            resource.getUsername() != null ? resource.getUsername() : username,
-                            resource.getDatabase() != null ? resource.getDatabase() : "test",
-                            resource.getPassword() != null ? resource.getPassword() : password.toCharArray()
-                    ),
-                    resource.getOptions()
-            );
-            database = resource.getDatabase();
+            host = uri.getHost();
+            port = uri.getPort();
         } catch (URISyntaxException exception) {
             log.error(exception.getMessage());
         }
+
+        // 参数或 url 其中一个不为空则使用参数或 url
+        if ((username == null || password == null)
+                && (resource.getUsername() == null || resource.getPassword() == null)) {
+            log.warn("No username or password provided. " +
+                    "This is not secure, and it is very likely that the physical server " +
+                    "can be compromised without the firewall being turned on.");
+            connection = new MongoClient(new ServerAddress(host, port), resource.getOptions());
+        } else {
+            connection = new MongoClient(
+                    new ServerAddress(host, port),
+                    // url 的用户名和密码 或 参数的用户名和密码
+                    // 按配置文件说明 url 的用户名和密码可不填写 故首选读取参数
+                    MongoCredential.createCredential(
+                            username != null ? username : resource.getUsername(),
+                            resource.getDatabase() != null ? resource.getDatabase() : "test",
+                            password != null ? password.toCharArray() : resource.getPassword()
+                    ),
+                    resource.getOptions()
+            );
+        }
+
+        database = resource.getDatabase();
     }
 
     @Override
