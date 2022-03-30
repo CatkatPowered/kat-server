5866df25c4854be95f33ef7e7b3ff0e3ff87b61c
SUIBING112233 <suibing112233@outlook.com>
2022-03-30 21:44:35 +0800

    ✨ feat:  Add config sections; Add tokenpool

--- src/main/java/com/catkatpowered/katserver/common/constants/KatConfigNodeConstants.java
+++ src/main/java/com/catkatpowered/katserver/common/constants/KatConfigNodeConstants.java
@@ -2,9 +2,15 @@ package com.catkatpowered.katserver.common.constants;
 
 public class KatConfigNodeConstants {
 
-    public static final String KAT_CONFIG_DATA_FOLDER_PATH = "data_folder_path";
+    public static final String KAT_CONFIG_RESOURCE = "resource:";
+    public static final String KAT_CONFIG_DATA_FOLDER_PATH = KAT_CONFIG_RESOURCE + "." + "data_folder_path";
 
-    public static final String KAT_CONFIG_NETWORK_PORT = "network_port";
+    public static final String KAT_CONFIG_NETWORK = "network";
+    public static final String KAT_CONFIG_NETWORK_PORT = KAT_CONFIG_NETWORK + "." + "network_port";
 
-    public static final String KAT_CONFIG_EXEC_THREADS = "exec_threads";
+    public static final String KAT_CONFIG_EXEC = "exec";
+    public static final String KAT_CONFIG_EXEC_THREADS = KAT_CONFIG_EXEC + "." + "exec_threads";
+
+    public static final String KAT_CONFIG_TOKENPOOL = "tokenpool";
+    public static final String KAT_CONFIG_TOKENPOOL_OUTDATE = KAT_CONFIG_TOKENPOOL + "." + "outdate";
 }
