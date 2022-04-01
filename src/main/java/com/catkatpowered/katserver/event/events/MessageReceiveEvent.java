d8e6e299b62502a09a2ae2881670bb705037fb24
SUIBING112233 <suibing112233@outlook.com>
2022-04-01 17:37:10 +0800

    🐛 Fix: Add @EqualsAndHashCode

--- src/main/java/com/catkatpowered/katserver/event/events/MessageReceiveEvent.java
+++ src/main/java/com/catkatpowered/katserver/event/events/MessageReceiveEvent.java
@@ -2,13 +2,17 @@ package com.catkatpowered.katserver.event.events;
 
 import com.catkatpowered.katserver.event.Event;
 import com.catkatpowered.katserver.message.KatUniMessage;
+
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class MessageReceiveEvent extends Event {
     String extensionId;
     KatUniMessage message;
+
     @Builder
     public MessageReceiveEvent(String extensionId, KatUniMessage message) {
         this.extensionId = extensionId;
--- src/main/java/com/catkatpowered/katserver/event/events/MessageSendEvent.java
+++ src/main/java/com/catkatpowered/katserver/event/events/MessageSendEvent.java
@@ -2,13 +2,17 @@ package com.catkatpowered.katserver.event.events;
 
 import com.catkatpowered.katserver.event.Event;
 import com.catkatpowered.katserver.message.KatUniMessage;
+
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class MessageSendEvent extends Event {
     private static String extensionId;
     private static KatUniMessage message;
+
     @Builder
     public MessageSendEvent(String extensionId, KatUniMessage message) {
         this.extensionId = extensionId;
