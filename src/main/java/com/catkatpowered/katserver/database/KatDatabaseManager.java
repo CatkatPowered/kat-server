75db0ecc82f1cfccd93843ec64d4638ed11ff20d
hanbings <3219065882@qq.com>
2022-04-02 15:32:34 +0800

    🐛 Fix: Changed mongodb database config.

--- src/main/java/com/catkatpowered/katserver/database/KatDatabaseManager.java
+++ src/main/java/com/catkatpowered/katserver/database/KatDatabaseManager.java
@@ -1,9 +1,13 @@
 package com.catkatpowered.katserver.database;
 
+import com.catkatpowered.katserver.KatServer;
+import com.catkatpowered.katserver.common.constants.KatConfigNodeConstants;
 import com.catkatpowered.katserver.database.interfaces.DatabaseConnector;
+import com.catkatpowered.katserver.database.mongodb.MongodbConnector;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * 数据库处理层 桥接数据库
@@ -16,6 +20,20 @@ public class KatDatabaseManager {
     @SuppressWarnings("ResultOfMethodCallIgnored")
     public static void init() {
         KatDatabase.getInstance();
+        
+        // 丑炸了 XD
+        @SuppressWarnings("OptionalGetWithoutIsPresent")
+        String url = KatServer.KatConfigAPI
+                .<String>getConfig(KatConfigNodeConstants.KAT_CONFIG_DATABASE_URL).get();
+        @SuppressWarnings("OptionalGetWithoutIsPresent")
+        String username = KatServer.KatConfigAPI
+                .<String>getConfig(KatConfigNodeConstants.KAT_CONFIG_DATABASE_USER_NAME).get();
+        @SuppressWarnings("OptionalGetWithoutIsPresent")
+        String password = KatServer.KatConfigAPI
+                .<String>getConfig(KatConfigNodeConstants.KAT_CONFIG_DATABASE_PASSWORD).get();
+
+        // 初始化数据库连接器
+        KatDatabaseManager.register(new MongodbConnector(url, username, password));
     }
 
     public static void register(DatabaseConnector connector) {
