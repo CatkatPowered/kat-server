ff78b1a6cb8c893a8c00b6f25932da8e6ae3c645
hanbings <3219065882@qq.com>
2022-04-08 19:46:20 +0800

    🔨 Refact: Add search() database interfaces.

--- src/main/java/com/catkatpowered/katserver/database/interfaces/DatabaseConnection.java
+++ src/main/java/com/catkatpowered/katserver/database/interfaces/DatabaseConnection.java
@@ -47,4 +47,19 @@ public interface DatabaseConnection {
      * @return 数据实体列表
      */
     <T> List<T> read(String collection, Map<String, Object> index, Class<T> type);
+
+    /**
+     * 搜索数据集合 符合的结果将进入 List 列表返回 需要提供泛型数据实体类类型
+     * 本方法提供根据某一个字段名称的边界以及限制数量进行搜索的操作
+     *
+     * @param collection 目标集合
+     * @param data       字段名称
+     * @param top        边界
+     * @param bottom     边界
+     * @param limit      限制值
+     * @param type       泛型数据实体类类型
+     * @param <T>        返回的数据实体类型
+     * @return 数据实体列表
+     */
+    <T> List<T> search(String collection, String data, String top, String bottom, int limit, Class<T> type);
 }
