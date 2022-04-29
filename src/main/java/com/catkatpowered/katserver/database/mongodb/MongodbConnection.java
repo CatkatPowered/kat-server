6839f504410a67f16fb2d978c54602d78f789451
hanbings <3219065882@qq.com>
2022-04-29 17:00:56 +0800

    🐛 Fix: Waste of resources caused by ORM writing problems.

--- src/main/java/com/catkatpowered/katserver/database/mongodb/MongodbConnection.java
+++ src/main/java/com/catkatpowered/katserver/database/mongodb/MongodbConnection.java
@@ -4,39 +4,45 @@ import com.catkatpowered.katserver.database.interfaces.DatabaseConnection;
 import com.google.gson.Gson;
 import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
-import org.bson.BsonDocument;
 import org.bson.Document;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public record MongodbConnection(Gson gson, MongoClient client, String database) implements DatabaseConnection {
 
     @Override
     public void create(String collection, Object data) {
-        client.getDatabase(database).getCollection(collection).insertOne(Document.parse(gson.toJson(data)));
+        client.getDatabase(database)
+                .getCollection(collection)
+                .insertOne(Document.parse(gson.toJson(data)));
     }
 
     @Override
     public void create(String collection, List<Object> data) {
-        data.forEach(d -> client.getDatabase(database)
+        client.getDatabase(database)
                 .getCollection(collection)
-                .insertOne(Document.parse(gson.toJson(d))));
+                .insertMany(data.stream()
+                        .map(d -> Document.parse(gson.toJson(d)))
+                        .collect(Collectors.toList()));
     }
 
     @Override
     public void update(String collection, Map<String, Object> index, Object data) {
-        client.getDatabase(database).getCollection(collection).updateMany(
-                Document.parse(gson.toJsonTree(index, Map.class).toString()),
-                Document.parse(gson.toJson(data))
-        );
+        client.getDatabase(database)
+                .getCollection(collection)
+                .updateMany(
+                        Document.parse(gson.toJsonTree(index, Map.class).toString()),
+                        Document.parse(gson.toJson(data))
+                );
     }
 
     @Override
     public void delete(String collection, Map<String, Object> index) {
-        client.getDatabase(database).getCollection(collection)
+        client.getDatabase(database)
+                .getCollection(collection)
                 .deleteMany(Document.parse(gson.toJsonTree(index, Map.class).toString()));
     }
 
@@ -44,7 +50,8 @@ public record MongodbConnection(Gson gson, MongoClient client, String database) 
     public <T> List<T> read(String collection, Map<String, Object> index, Class<T> type) {
         List<T> list = new ArrayList<>();
         // 获取数据库连接
-        client.getDatabase(database).getCollection(collection)
+        client.getDatabase(database)
+                .getCollection(collection)
                 .find(Document.parse(gson.toJsonTree(index, Map.class).toString()))
                 .iterator()
                 .forEachRemaining(document -> {
@@ -58,7 +65,8 @@ public record MongodbConnection(Gson gson, MongoClient client, String database) 
     public <T, V> List<T> search(String collection, String data, V top, V bottom, int limit, Class<T> type) {
         List<T> list = new ArrayList<>();
         // 获取数据库连接
-        client.getDatabase(database).getCollection(collection)
+        client.getDatabase(database)
+                .getCollection(collection)
                 .find(new BasicDBObject() {{
                     put(data, new BasicDBObject() {{
                         put("$gte", top);
