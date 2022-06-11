bff75bfe0949809b5c10b3716accc3c40c1a2323
Krysztal <suibing112233@outlook.com>
2022-06-11 23:45:18 +0800

    :sparkles: Feat: Finished LocalProvider and KatStorageProvider design,without unit test suits.

--- src/main/java/com/catkatpowered/katserver/storage/providers/KatStorageProvider.java
+++ src/main/java/com/catkatpowered/katserver/storage/providers/KatStorageProvider.java
@@ -1,52 +1,54 @@
 package com.catkatpowered.katserver.storage.providers;
 
+import com.catkatpowered.katserver.common.utils.KatShaUtils;
+
 import java.util.Optional;
 
+/**
+ * @author Krysztal
+ */
 public abstract class KatStorageProvider {
 
     protected KatStorageProvider() {
     }
 
     /**
-     * 
      * 获取资源
-     * 
-     * @param resource 包含资源信息的数据结构
+     *
+     * @param hashString 为想要获取文件的 Hash 值，约定使用 {@link KatShaUtils} 计算 Hash
      * @return 由 <b>Optional</b> 容器包装的包含资源文件的具体信息
+     * @see KatShaUtils
      */
-    public abstract Optional<KatResource> fetch(KatResource resource);
+    public abstract Optional<KatResource> fetch(String hashString);
 
     /**
-     * 
-     * 校验资源
-     * 
+     * 使用 Hash 校验资源，校验工具为 {@link KatShaUtils}
+     *
      * @param resource 包含资源信息的数据结构
      * @return 由 <b>Optional</b> 容器包装的包含资源文件的具体信息
+     * @see KatShaUtils
      */
-    public abstract Optional<KatResource> validate(KatResource resource);
+    public abstract Optional<KatResource> validate(String hashString);
 
     /**
-     * 
      * 上传资源
-     * 
+     *
      * @param resource 包含资源信息的数据结构
      * @return 由 <b>Optional</b> 容器包装的包含资源文件的具体信息
      */
     public abstract Optional<KatResource> upload(KatResource resource);
 
     /**
-     * 
      * 删除资源
-     * 
+     *
      * @param resource 包含资源信息的数据结构
-     * @return 由 <b>Optional</b> 容器包装的包含资源文件的具体信息
+     * @return 是否成功删除
      */
-    public abstract Optional<KatResource> delete(KatResource resource);
+    public abstract boolean delete(String hashString);
 
     /**
-     * 
      * 更新资源
-     * 
+     *
      * @param resource 包含资源信息的数据结构
      * @return 由 <b>Optional</b> 容器包装的包含资源文件的具体信息
      */
--- src/main/java/com/catkatpowered/katserver/storage/providers/local/LocalProvider.java
+++ src/main/java/com/catkatpowered/katserver/storage/providers/local/LocalProvider.java
@@ -1,59 +1,73 @@
 package com.catkatpowered.katserver.storage.providers.local;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.util.Optional;
-
 import com.catkatpowered.katserver.KatServer;
 import com.catkatpowered.katserver.common.constants.KatConfigNodeConstants;
+import com.catkatpowered.katserver.common.utils.KatShaUtils;
 import com.catkatpowered.katserver.storage.providers.KatResource;
 import com.catkatpowered.katserver.storage.providers.KatStorageProvider;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * @author Krysztal
+ */
 public class LocalProvider extends KatStorageProvider {
 
     private final String resourceStoragePath;
 
     public LocalProvider() {
         super();
-        resourceStoragePath = KatServer.KatConfigAPI
-                .<String>getConfig(KatConfigNodeConstants.KAT_CONFIG_RESOURCE_DATA_FOLDER_PATH)
-                .get();
+        resourceStoragePath = KatServer.KatConfigAPI.<String>getConfig(KatConfigNodeConstants.KAT_CONFIG_RESOURCE_DATA_FOLDER_PATH).orElse("./data");
     }
 
     /**
-     * 获取资源，其中 URI字段为
-     * 
-     * 
+     * 通过文件 Hash 获取资源文件
+     *
+     * @return URI 字段为文件于磁盘中的位置
+     * @see java.net.URI
+     * @see Optional
+     * @see com.catkatpowered.katserver.common.utils.KatShaUtils
      */
     @Override
-    public Optional<KatResource> fetch(KatResource resource) {
-        var path = hashToPath(resource);
+    public Optional<KatResource> fetch(String hashString) {
+        var path = toPath(hashString);
+        if (path.isEmpty()) return Optional.empty();
+
         var file = new File(path.get());
+        if (!(new File(path.get())).exists()) return Optional.empty();
 
-        if (path.isEmpty() || (!(new File(path.get())).exists()))
-            return Optional.empty();
-
-        return Optional.of(KatResource
-                .builder()
-                .hash(resource.hash)
-                .size(file.length())
-                .uri(file.toURI())
-                .build());
+        return Optional.of(KatResource.builder().hash(hashString).size(file.length()).uri(file.toURI()).build());
     }
 
     @Override
-    public Optional<KatResource> validate(KatResource resource) {
-        return Optional.empty();
+    public Optional<KatResource> validate(String hashString) {
+        // 判断是否存在文件，若不存在则返回空 Optional
+        var fetchedKatResource = this.fetch(hashString);
+        if (fetchedKatResource.isEmpty()) return Optional.empty();
+
+        // 由于是本地文件，因此直接计算文件Sha256
+        var fileURI = fetchedKatResource.get().getUri();
+        var fileSha256 = KatShaUtils.sha256(new File(fileURI));
+
+        return !Objects.equals(hashString, fileSha256) ? Optional.empty() : fetchedKatResource;
     }
 
+    // TODO: 等待DownloadUtil
     @Override
     public Optional<KatResource> upload(KatResource resource) {
         return Optional.empty();
     }
 
     @Override
-    public Optional<KatResource> delete(KatResource resource) {
-        return Optional.empty();
+    public boolean delete(String hashString) {
+        var fetchedOptionalKatResource = this.fetch(hashString);
+        if (fetchedOptionalKatResource.isEmpty()) return false;
+
+        var file = new File(fetchedOptionalKatResource.get().getUri());
+        return file.delete();
     }
 
     @Override
@@ -61,12 +75,8 @@ public class LocalProvider extends KatStorageProvider {
         return Optional.empty();
     }
 
-    private Optional<String> hashToPath(KatResource resource) {
-        return Optional.of(Path.of(
-                this.resourceStoragePath,
-                resource.hash.substring(0, 2))
-                .toAbsolutePath()
-                .toString());
+    private Optional<String> toPath(String hashString) {
+        return Optional.of(Path.of(this.resourceStoragePath, hashString.substring(0, 2)).toAbsolutePath().toString());
     }
 
 }
