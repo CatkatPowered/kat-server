be4d3d840952d79886ea8ebd598a881faebadbd0
Krysztal <suibing112233@outlook.com>
2022-06-16 17:21:42 +0800

    :hammer: Fix: Fix TestKatConfig.java

--- src/test/java/com/catkatpowered/katserver/config/TestKatConfig.java
+++ src/test/java/com/catkatpowered/katserver/config/TestKatConfig.java
@@ -12,82 +12,78 @@ import com.catkatpowered.katserver.KatServer;
 import com.catkatpowered.katserver.common.constants.KatConfigNodeConstants;
 
 public class TestKatConfig {
-    String defaultConfig = """
-                ######################### Network #############################
-                network:
-                  network_port: 25565
+  String defaultConfig = """
+      ######################### Network #############################
+      network:
+        network_port: 25565
+        selfgen_cert:
+          cert_password: "catmoe"
+          cert_alias: "catmoe"
+        custom_cert:
+          enabled: false
+          cert_path: "./cert.jks"
+          cert_password: "catmoe"
 
-                ######################### Database ############################
-                database:
-                  # The SQLite only for development!
-                  #
-                  # You can choose those type of database:
-                  # 1. postgresql
-                  # 2. mysql
-                  # 3. mongodb
-                  # 4. sqlite
-                  database_type: sqlite
+      ######################### Database ############################
+      database:
+        # default support mongodb
+        # demo database_url:
+        # mongodb: mongodb://localhost:27017/database_name
+        #
+        # Note: No need to carry username and password in url
+        database_url: mongodb://localhost:27017/kat-server
+        database_username:
+        database_password:
 
-                  # If you choose sqlite, you don't have to add anything auth.
-                  # Twice, it only for development!
-                  # Use mysql postgresql or mongodb you need to add auth.
-                  #
-                  # demo database_url:
-                  # postgresql: postgresql://localhost:5432/database_name
-                  # mysql: mysql://localhost:3306/database_name
-                  # mongodb: mongodb://localhost:27017/database_name
-                  #
-                  # Note: No need to carry username and password in url
-                  database_url:
-                  database_username:
-                  database_password:
+      ####################### Storage ###############################
+      # The resource file storage
+      resource:
+        # You can choose those type of storage_provider
+        # 1.local
+        storage_provider: local
 
-                ####################### Storage ###############################
-                # The resource file storage
-                resource:
-                  # You can choose those type of resource_storage
-                  # 1.local
-                  resource_storage: local
+        # If you choose `local`,
+        # you can set the resource file where to store.
+        data_folder_path: "./data"
 
-                  # If you choose `local`,
-                  # you can set the resource file where to store.
-                  data_folder_path: "./data"
+      ####################### ExecThreads ############################
+      exec:
+        exec_threads: 16
 
-                ####################### ExecThreads ############################
-                exec:
-                  exec_threads: 16
+      ####################### TokenPool ##############################
+      # This section are the settings of token pool
 
-                ####################### TokenPool ##############################
-                # This section are the settings of token pool
+      tokenpool:
+        # You can set the token how long to live
+        #
+        # Unit: Millisecond
+        outdated: 10000
 
-                tokenpool:
-                  # You can set the token how long to live
-                  outdate: 10
             """;
 
-    @Test
-    public void testConfigNode() {
-        Map<String, Object> yml = new Yaml().load(defaultConfig);
-        KatConfig.getInstance().setConfigContent(yml);
+  @Test
+  public void testConfigNode() {
+    Map<String, Object> yml = new Yaml().load(defaultConfig);
+    KatConfig.getInstance().setConfigContent(yml);
 
-        // Get undefined or null config node
-        var undefinedNode = KatServer.KatConfigAPI.getConfig("undefined");
-        assertEquals(Optional.empty(), undefinedNode);
+    // Get undefined or null config node
+    var undefinedNode = KatServer.KatConfigAPI.getConfig("undefined");
+    assertEquals(Optional.empty(), undefinedNode);
 
-        var nullNode = KatServer.KatConfigAPI.getConfig("");
-        assertEquals(Optional.empty(), nullNode);
+    var nullNode = KatServer.KatConfigAPI.getConfig("");
+    assertEquals(Optional.empty(), nullNode);
 
-        var nullConfigNode = KatServer.KatConfigAPI.getConfig(null); // 炒饭
-        assertEquals(Optional.empty(), nullConfigNode);
+    var nullConfigNode = KatServer.KatConfigAPI.getConfig(null); // 炒饭
+    assertEquals(Optional.empty(), nullConfigNode);
 
-        // Try worn type cast
-        KatServer.KatConfigAPI.<Test>getConfig(KatConfigNodeConstants.KAT_CONFIG_DATABASE); // 炒饭，酒吧没炸就行
+    // Try worn type cast
+    KatServer.KatConfigAPI.<Test>getConfig(KatConfigNodeConstants.KAT_CONFIG_DATABASE); // 炒饭，酒吧没炸就行
 
-        // Get already exist config node
-        var network = KatServer.KatConfigAPI.<Map<String, Object>>getConfig(KatConfigNodeConstants.KAT_CONFIG_NETWORK);
-        assertEquals(25565, network.get().get("network_port"));
+    // Get already exist config node
+    var network = KatServer.KatConfigAPI.<Map<String, Object>>getConfig(KatConfigNodeConstants.KAT_CONFIG_NETWORK);
+    assertEquals(25565, network.get().get("network_port"));
 
-        var networkPort = KatServer.KatConfigAPI.<Integer>getConfig(KatConfigNodeConstants.KAT_CONFIG_NETWORK_PORT);
-        assertEquals(25565, networkPort.get());
-    }
+    var networkPort = KatServer.KatConfigAPI.<Integer>getConfig(KatConfigNodeConstants.KAT_CONFIG_NETWORK_PORT);
+    assertEquals(25565, networkPort.get());
+  }
 }
